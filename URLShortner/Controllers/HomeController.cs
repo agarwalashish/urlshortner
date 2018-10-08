e748ea03189e245229c35139fc5d5952ab3ecb60
Ashish Agarwal <iamashish.agarwal@gmail.com>
2018-10-08 01:04:34 -0700

    created api controller

---
+++ URLShortner/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace URLShortner.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("shorten")]
+        public async Task<IActionResult> ShortenURLAsync([FromQuery] string url)
+        {
+            return Ok(url);
+        }
+    }
+}
